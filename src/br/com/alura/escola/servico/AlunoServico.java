c2d94bf1550710d3a27f9d21f2b4fe29cf3b5e27
João Victor <jvc.martins12@gmail.com>
2019-10-14 22:15:07 -0300

    Commit do código da primeira aula

---
+++ src/br/com/alura/escola/servico/AlunoServico.java
@@ -0,0 +1,19 @@
+package br.com.alura.escola.servico;
+
+import java.util.List;
+import br.com.alura.escola.modelo.Aluno;
+
+public class AlunoServico {
+
+	Aluno priscila = new Aluno("Priscila", 7670832029L);
+	Aluno fernando = new Aluno("Fernando", 3588807004L);
+	Aluno rafael = new Aluno("Rafael", 49157745030L);
+	Aluno renata = new Aluno("Renata", 82757618083L);
+	Aluno aluno = new Aluno(null, 41189989042L);
+
+	public List<Aluno> listar() {
+		List<Aluno> alunos = List.of(priscila, fernando, rafael, aluno);
+		alunos.add(renata);
+		return alunos;
+	}
+}
