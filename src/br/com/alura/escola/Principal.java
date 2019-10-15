8321ce22852b9c3ee433f67f1a0f5deccfdc8995
João Victor <jvc.martins12@gmail.com>
2019-10-14 22:38:09 -0300

    Commit do código do terceiro vídeo

--- src/br/com/alura/escola/Principal.java
+++ src/br/com/alura/escola/Principal.java
@@ -1,16 +1,22 @@
 package br.com.alura.escola;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import br.com.alura.escola.modelo.Curso;
+import br.com.alura.escola.modelo.Turma;
 import br.com.alura.escola.servico.AlunoServico;
+import br.com.alura.escola.servico.TurmaServico;
 
 public class Principal {
 
 	public static void main(String...strings) {
 
 		AlunoServico alunoServico = new AlunoServico();
+		TurmaServico turmaServico = new TurmaServico();
 
 		List<String> alunos = alunoServico.listar().stream()
 				.flatMap(a -> Stream.ofNullable(a.getNome()))
@@ -18,5 +24,11 @@ public class Principal {
 				.collect(Collectors.toList());
 
 		System.out.println("Lista de alunos matriculados na escola: " + alunos);
+
+		Map<Curso, List<Turma>> turmasPorCurso = turmaServico.listar().stream()
+				.filter(a -> LocalDate.of(2019, 4, 3).equals(a.getInicio()))
+				.collect(Collectors.groupingBy(Turma::getCurso));
+
+		System.out.println("Relação de turmas por curso: " + turmasPorCurso);
 	}
 }
